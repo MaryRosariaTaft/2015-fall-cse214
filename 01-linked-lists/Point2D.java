321cca110e1c205c6f333f97b3fada6c1d66a3d0
Mary Taft <Mary.Taft97@gmail.com>
2015-09-17 18:32:02 -0400

    framework

--- 01-linked-lists/Point2D.java
+++ 01-linked-lists/Point2D.java
@@ -3,6 +3,44 @@ import java.util.*;
 
 public class Point2D{
 
-    //woooooo
+    //calculates Euclidean distance between p and q
+    public static double distance(OrderedDoublePair p, OrderedDoublePair q){
+
+    }
+
+    //converts array to UniLinkedList
+    public static UniLinkedList<OrderedDoublePair> fromArray(OrderedDoublePair[] points){
+    
+    }
+
+    //converts 2D array to 2D UniLinkedList
+    public static UniLinkedList<OrderedDoublePair> from2DArray(double[][] points){
+
+    }
+
+    //computes centroid (arithmetic mean of all points in the given list)
+    public static OrderedDoublePair centroid(UniLinkedList<OrderedDoublePair> points){
+
+    }
+
+    //returns point closest to the origin
+    public static OrderedDoublePair smallest(UniLinkedList<OrderedDoublePair> points){
+	
+    }
+
+    //returns point farthest from the origin
+    public static OrderedDoublePair largest(UniLinkedList<OrderedDoublePair> points){
+
+    }
+
+    //reads command-line input in the form "x y" (line by line) until user types "done"
+    //compute centroid
+    //remove duplicates
+    //find nearest and farthest points
+    //remove all points from original list whose coordinates sum to an even number
+    //print point closest to centroid after deduplication (huh...)
+    // public static void main(String[] args){
+
+    // }
 
 }
