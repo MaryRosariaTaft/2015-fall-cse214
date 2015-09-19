7a7323ce6bdf9682d572baa937b28a58da0838c2
Mary Taft <Mary.Taft97@gmail.com>
2015-09-19 13:30:06 -0400

    completed distance() and the two fromArray() methods

--- 01-linked-lists/Point2D.java
+++ 01-linked-lists/Point2D.java
@@ -5,31 +5,46 @@ public class Point2D{
 
     //calculates Euclidean distance between p and q
     public static double distance(OrderedDoublePair p, OrderedDoublePair q){
-
+	double x1 = p.getX(), y1 = p.getY(), x2 = q.getX(), y2 = q.getY();
+	return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)); //Java doesn't have the ** notation :'(
     }
-
+    
     //converts array to UniLinkedList
     public static UniLinkedList<OrderedDoublePair> fromArray(OrderedDoublePair[] points){
-    
+	UniLinkedList<OrderedDoublePair> ans = new UniLinkedList<OrderedDoublePair>();
+	for(int i = 0; i < points.length; i++){
+	    ans.add(points[i]);
+	}
+	return ans;
     }
 
     //converts 2D array to 2D UniLinkedList
+    //is it a problem to add "throws *Exception" to a method if
+    //it's not previously noted / called for in the API?
     public static UniLinkedList<OrderedDoublePair> from2DArray(double[][] points){
-
+	UniLinkedList<OrderedDoublePair> ans = new UniLinkedList<OrderedDoublePair>();
+	for(int i = 0; i < points.length; i++){
+	    if(points[i].length != 2)
+		throw new IllegalArgumentException("at index " + i + ": " + Arrays.toString(points[i]) + " is not a pair of coordinates");
+	    ans.add(new OrderedDoublePair(points[i][0], points[i][1]));
+	}
+	return ans;
     }
 
     //computes centroid (arithmetic mean of all points in the given list)
     public static OrderedDoublePair centroid(UniLinkedList<OrderedDoublePair> points){
-
+	return new OrderedDoublePair(0,0);
     }
 
     //returns point closest to the origin
     public static OrderedDoublePair smallest(UniLinkedList<OrderedDoublePair> points){
+	return new OrderedDoublePair(0,0);
 	
     }
 
     //returns point farthest from the origin
     public static OrderedDoublePair largest(UniLinkedList<OrderedDoublePair> points){
+	return new OrderedDoublePair(0,0);
 
     }
 
--- 01-linked-lists/Tester.java
+++ 01-linked-lists/Tester.java
@@ -3,96 +3,126 @@ public class Tester{
     public static void main(String[] args){
 
 	//ORDEREDDOUBLEPAIR TESTS
+
 	OrderedDoublePair a = new OrderedDoublePair();
 	OrderedDoublePair b = new OrderedDoublePair(1,1);
 	OrderedDoublePair c = new OrderedDoublePair(2.0,2.0);
-	print(a);
-	print(b);
-	print(c);
+	// print(a);
+	// print(b);
+	// print(c);
+	// print(a.equals(b));
+	// print(a.equals(new OrderedDoublePair(0,0)));
+	// print(new OrderedDoublePair(1,2).equalsIgnoreOrder(new OrderedDoublePair(2,1)));
+
 
 	//UNILINKEDLIST TESTS
-	/*
-	UniLinkedList<String> ll = new UniLinkedList<String>();
-	print("\n\nHello World\n\n");
-	print(ll.isEmpty());
-	ll.clear();
-	print(ll.size());
-	print(ll.head());
-	print(ll);
-	ll.add("hello");
-	print(ll);
-	ll.add("world");
-	print(ll);
-	ll.add("whoops");
-	print(ll);
-	print(ll.size());
-	print(ll.indexOf("whoops"));
-	print("-------");
-	print(ll.addAfter("hello", "dear"));
-	print(ll);
-	print(ll.addAfter("nonexistent","test"));
-	print(ll);
-	print(ll.contains("dear"));
-	print(ll.contains("nonexistent"));
-	print(ll.size());
-	print(ll.addFirst("alpha"));
-	print(ll);
-	print(ll.remove("whoops"));
-	print(ll);
-	print(ll.remove("hello"));
-	print(ll);
-	print(ll.remove("dear"));
-	print(ll);
-	print(ll.remove("world"));
-	print(ll);
-	ll.add("apple");
-	ll.add("apple");
-	ll.add("pear");
-	ll.add("apple");
-	ll.add("apple");
-	ll.add("pear");
-	ll.add("apple");
-	ll.add("apple");
-	ll.add("pear");	
-	print(ll);
-	print(ll.size());
-	print(ll.removeAll("apple"));
-	print(ll.removeAll("orange"));
-	print(ll); 
-	print(ll.size());
-	ll.add("apple");
-	ll.add("apple");
-	ll.add("pear");	
-	ll.add("apple");
-	ll.add("apple");
-	ll.add("pear");	
-	ll.add("apple");
-	ll.add("apple");
-	ll.add("pear");	
-	print(ll);
-	ll.clear();
-	print("JUST CLEARED");
-	print(ll);
-	ll.add("a");
-	ll.add("b");
-	//ll.add("c");
-	ll.add("d");
-	//ll.add("d");
-	print("DEDUPLICATING");
-	ll.deduplicate();
-	print(ll);
-	print(ll.size());
-	UniLinkedList<String> ll2 = new UniLinkedList<String>();
-	ll2.add("a");
-	ll2.add("b");
-	ll2.add("c");
-	ll2.add("d");
-	print("ll: " + ll);
-	print("ll2: " + ll2);
-	print(ll.equals(ll2));
-	*/
+
+	// UniLinkedList<String> ll = new UniLinkedList<String>();
+	// print("\n\nHello World\n\n");
+	// print(ll.isEmpty());
+	// ll.clear();
+	// print(ll.size());
+	// print(ll.head());
+	// print(ll);
+	// ll.add("hello");
+	// print(ll);
+	// ll.add("world");
+	// print(ll);
+	// ll.add("whoops");
+	// print(ll);
+	// print(ll.size());
+	// print(ll.indexOf("whoops"));
+	// print("-------");
+	// print(ll.addAfter("hello", "dear"));
+	// print(ll);
+	// print(ll.addAfter("nonexistent","test"));
+	// print(ll);
+	// print(ll.contains("dear"));
+	// print(ll.contains("nonexistent"));
+	// print(ll.size());
+	// print(ll.addFirst("alpha"));
+	// print(ll);
+	// print(ll.remove("whoops"));
+	// print(ll);
+	// print(ll.remove("hello"));
+	// print(ll);
+	// print(ll.remove("dear"));
+	// print(ll);
+	// print(ll.remove("world"));
+	// print(ll);
+	// ll.add("apple");
+	// ll.add("apple");
+	// ll.add("pear");
+	// ll.add("apple");
+	// ll.add("apple");
+	// ll.add("pear");
+	// ll.add("apple");
+	// ll.add("apple");
+	// ll.add("pear");	
+	// print(ll);
+	// print(ll.size());
+	// print(ll.removeAll("apple"));
+	// print(ll.removeAll("orange"));
+	// print(ll); 
+	// print(ll.size());
+	// ll.add("apple");
+	// ll.add("apple");
+	// ll.add("pear");	
+	// ll.add("apple");
+	// ll.add("apple");
+	// ll.add("pear");	
+	// ll.add("apple");
+	// ll.add("apple");
+	// ll.add("pear");	
+	// print(ll);
+	// ll.clear();
+	// print("JUST CLEARED");
+	// print(ll);
+	// ll.add("a");
+	// ll.add("b");
+	// ll.add("c");
+	// ll.add("d");
+	// ll.add("d");
+	// print("DEDUPLICATING");
+	// ll.deduplicate();
+	// print(ll);
+	// print(ll.size());
+	// UniLinkedList<String> ll2 = new UniLinkedList<String>();
+	// ll2.add("a");
+	// ll2.add("b");
+	// ll2.add("c");
+	// ll2.add("d");
+	// print("ll: " + ll);
+	// print("ll2: " + ll2);
+	// print(ll.equals(ll2));
+	// ll.remove("b");
+	// print(ll.equals(ll2));
+
 
 	//POINT2D TESTS
+
+	print(Point2D.distance(a, b));
+
+	OrderedDoublePair[] arraySet = {a,b,c};
+	UniLinkedList<OrderedDoublePair> llSet;
+	llSet = Point2D.fromArray(arraySet);
+	print(llSet);
+
+	OrderedDoublePair[] arraySet2 = {a,c,b,a};
+	UniLinkedList<OrderedDoublePair> llSet2;
+	llSet2 = Point2D.fromArray(arraySet2);
+	print(llSet2);
+
+	double[][] arraySet3 = {{07, 10}, {07, 04}, {12, 31}};
+	UniLinkedList<OrderedDoublePair> llSet3;
+	llSet3 = Point2D.from2DArray(arraySet3);
+	print(llSet3);
+
+	//works!  commenting out so no exception is thrown while testing other thigns
+	// double[][] arraySet4 = {{07, 04}, {07, 10, 01, 23}, {12, 31}};
+	// UniLinkedList<OrderedDoublePair> llSet4;
+	// llSet4 = Point2D.from2DArray(arraySet4);
+	// print(llSet4);
 
     }
 
