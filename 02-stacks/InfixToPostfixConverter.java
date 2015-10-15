1c9edd345e7d6b7477f0e231f6044175d555ed4e
Mary Taft <Mary.Taft97@gmail.com>
2015-10-14 23:57:15 -0400

    only works for fully parenthesized expressions and limited to single-digit numbers.  usefulness at its best.

--- 02-stacks/InfixToPostfixConverter.java
+++ 02-stacks/InfixToPostfixConverter.java
@@ -1,3 +1,30 @@
+import java.io.*;
+import java.util.*;
+
 public class InfixToPostfixConverter{
+
+    public static String convert(char[] infix) throws IllegalArgumentException{
+	String postfix = "";
+	Stack<Character> operators = new Stack<Character>();
+	for(int i = 0; i < infix.length; i++){
+	    if("0123456789".contains(String.valueOf(infix[i]))){
+		postfix += infix[i] + " ";
+	    }else if("+-*/".contains(String.valueOf(infix[i]))){
+		operators.push(infix[i]);
+	    }else if(infix[i] == ')'){
+		postfix += operators.pop() + " ";
+	    }else if(infix[i] == '('){
+		continue;
+	    }else{
+		throw new IllegalArgumentException();
+	    }
+	}
+	return postfix;
+    }
+
+    public static void main(String[] args){
+	char[] infix = {'(', '(', '2', '+', '3', ')', '*', ')'};
+	System.out.println(convert(infix));
+    }
 
 }
